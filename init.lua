7b605a29b9fe51b272606f82e3cfd5cb94e6c784
Kenneth Yang <microbob@is88.com>
2025-08-14 15:46:06 -0700

    Options file with line numbering

---
+++ init.lua
@@ -0,0 +1,5 @@
+-- Set <space> as the leader key and <comma> as the local leader
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ','
+
+require "options"
---
+++ lua/options.lua
@@ -0,0 +1,8 @@
+-- [[ Editor ]]
+
+-- Fonts.
+vim.g.have_nerd_font = true
+
+-- Line numbering.
+vim.o.number = true
+vim.o.relativenumber = true
