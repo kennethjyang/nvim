5cd25db3bb61bade1fa5a1185ba69c4dd861ddf1
Kenneth Yang <microbob@is88.com>
2025-08-14 16:29:28 -0700

    Init lazy

--- init.lua
+++ init.lua
@@ -3,3 +3,4 @@ vim.g.mapleader = ' '
 vim.g.maplocalleader = ','
 
 require "options"
+require "lazy-pm"
--- lua/options.lua
+++ lua/options.lua
@@ -6,3 +6,14 @@ vim.g.have_nerd_font = true
 -- Line numbering.
 vim.o.number = true
 vim.o.relativenumber = true
+
+-- Line
+vim.o.cursorline = true
+vim.o.wrap = true
+
+-- Indentation
+vim.o.tabstop = 4
+vim.o.shiftwidth = 4
+vim.o.expandtab = true
+vim.o.smartindent = true
+vim.o.autoindent = true
