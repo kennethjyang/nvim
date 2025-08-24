b812b50e2ee5fb3a9905ff44422cee08b8dbf50c
Kenneth Yang <microbob@is88.com>
2025-08-23 19:31:28 -0700

    Moved scaling options to keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -29,6 +29,23 @@ if vim.g.neovide then
   vim.keymap.set('i', '<D-v>', '<ESC>l"+Pli') -- Paste insert mode
 end
 
+-- Neovide scaling
+if vim.g.neovide then
+  vim.g.neovide_scale_factor = 1.0
+  local change_scale_factor = function(delta)
+    vim.g.neovide_scale_factor = vim.g.neovide_scale_factor * delta
+  end
+  vim.keymap.set("n", "<C-=>", function()
+    change_scale_factor(1.25)
+  end)
+  vim.keymap.set("n", "<C-->", function()
+    change_scale_factor(1 / 1.25)
+  end)
+  vim.keymap.set("n", "<C-0>", function()
+    vim.g.neovide_scale_factor = 1.0
+  end)
+end
+
 -- Allow clipboard copy paste in neovim
 vim.api.nvim_set_keymap('', '<D-v>', '+p<CR>', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('!', '<D-v>', '<C-R>+', { noremap = true, silent = true })
--- lua/config/options.lua
+++ lua/config/options.lua
@@ -42,21 +42,6 @@ if vim.g.neovide then
   vim.cmd [[colorscheme tokyonight]]
   vim.o.guifont = "JetBrainsMono Nerd Font"
   vim.g.neovide_theme = 'auto'
-
-  -- Scaling
-  vim.g.neovide_scale_factor = 1.0
-  local change_scale_factor = function(delta)
-    vim.g.neovide_scale_factor = vim.g.neovide_scale_factor * delta
-  end
-  vim.keymap.set("n", "<C-=>", function()
-    change_scale_factor(1.25)
-  end)
-  vim.keymap.set("n", "<C-->", function()
-    change_scale_factor(1 / 1.25)
-  end)
-  vim.keymap.set("n", "<C-0>", function()
-    vim.g.neovide_scale_factor = 1.0
-  end)
 end
 
 -- [[ Editing Features ]]
