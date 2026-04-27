852566f0ee1b7c7f071bb4ae8f88fd92946212e5
Kenneth Yang <microbob@is88.com>
2026-04-27 14:02:11 -0700

    Disable system clipboard due to lag

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -17,7 +17,7 @@ vim.o.showmatch = true
 vim.o.mousescroll = "ver:1,hor:1"
 
 -- Use system clipboard.
-vim.o.clipboard = "unnamedplus"
+-- vim.o.clipboard = "unnamedplus"
 
 -- Lower update time to modern standards.
 vim.o.updatetime = 250
